2ce07c3e90ba5906d92c0bb851119f0573b0ceb5
Jens Ahrengot Boddum <ahrengot@gmail.com>
2023-08-02 11:29:41 +0200

    Register install command that publishes enum stubs, migrations and config file

--- src/RolesAndPermissionsServiceProvider.php
+++ src/RolesAndPermissionsServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace Ahrengot\RolesAndPermissions;
 
-use Ahrengot\RolesAndPermissions\Commands\RolesAndPermissionsCommand;
+use Illuminate\Support\Facades\Artisan;
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -16,10 +17,34 @@ class RolesAndPermissionsServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package
-            ->name('laravel-roles-and-permissions')
+            ->name('roles-and-permissions')
             ->hasConfigFile('permissions')
-//            ->hasViews()
-            ->hasMigration('add_role_to_users');
-        //            ->hasCommand(RolesAndPermissionsCommand::class);
+            ->hasMigration('add_role_to_users')
+            ->hasInstallCommand(function (InstallCommand $command) {
+                $command
+                    ->startWith(
+                        fn () => $this->publishEnums($command)
+                    )
+                    ->publishConfigFile()
+                    ->publishMigrations()
+                    ->endWith(
+                        fn (InstallCommand $command) => $command->info('Package installed. Remember to update your User model with the provided HasPermissions trait and an enum cast for the role column.')
+                    );
+            });
+    }
+
+    public function bootingPackage()
+    {
+        $this->publishes([
+            $this->package->basePath('../resources/stubs/Enums/UserRole.php.stub') => base_path('app/Enums/UserRole.php'),
+            $this->package->basePath('../resources/stubs/Enums/Permission.php.stub') => base_path('app/Enums/Permission.php'),
+        ], "{$this->package->shortName()}-enums");
+    }
+
+    public function publishEnums(InstallCommand $command): void
+    {
+        Artisan::call('vendor:publish', [
+            '--tag' => "{$this->package->shortName()}-enums",
+        ], $command->getOutput());
     }
 }
